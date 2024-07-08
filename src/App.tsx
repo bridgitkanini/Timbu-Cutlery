7bbf665bae356411d3dfe5c6430f3e2723c1c9d4
bkm-code <bridgitmutinda2015@gmail.com>
2024-07-08 23:39:27 +0300

    Configure page routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import Home from "./components/Home"
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar.tsx";
+import Footer from "./components/Footer.tsx";
+import Home from "./components/Home";
+import Product from "./components/Product.tsx";
 
 function App() {
-
   return (
-    <>
+    <Router>
       <div className="max-w-[430px] md:max-w-[1440px] m-auto ">
-        <Home />
+        <Navbar />
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/product" Component={Product} />
+        </Routes>
+        <Footer />
       </div>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
