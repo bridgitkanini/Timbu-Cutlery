63356bb95106a5266a791dcee164a61b1dcb6c1f
bkm-code <bridgitmutinda2015@gmail.com>
2024-07-09 05:15:15 +0300

    Logo edit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="max-w-[430px] md:max-w-[1440px] m-8 my-10">
       <div className="flex flex-row justify-between w-full">
-        <p className="hidden sm:block font-bold">TIMBU LTD.</p>
+        <p className="hidden sm:block font-bold">TIMBU Ltd.</p>
         <p className="text-gray-500">© 2024. All rights reserved.</p>
       </div>
     </div>
