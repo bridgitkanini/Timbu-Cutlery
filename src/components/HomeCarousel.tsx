ab5de5cf0fbf5171f1f295ab848081e9112e4b7e
bkm-code <bridgitmutinda2015@gmail.com>
2024-07-09 17:04:33 +0300

    Add discount tag on homepage carousel

--- src/components/HomeCarousel.tsx
+++ src/components/HomeCarousel.tsx
@@ -2,10 +2,18 @@ import CarouselComponent from "./CarouselComponent";
 import Carousel1 from "./../assets/images/Carousel image 1.png";
 import Carousel2 from "./../assets/images/Carousel image 2.png";
 import Carousel3 from "./../assets/images/Carousel image 3.png";
+import Tag from './../assets/images/Tag.png';
 
 
 const HomeCarousel = () => (
-  <CarouselComponent images={[Carousel1, Carousel2, Carousel3]} />
+  <div className="relative">
+    <img
+      src={Tag}
+      alt="tag"
+      className="absolute top-[120px] left-[-50px] sm:top-[200px] sm:left-[70px] sm:w-[15%] z-10"
+    />
+    <CarouselComponent images={[Carousel1, Carousel2, Carousel3]} />
+  </div>
 );
 
 
