89b363ded1af82d57c1a686937932360bdda4f5a
bkm-code <bridgitmutinda2015@gmail.com>
2024-07-09 05:14:41 +0300

    Added new pages routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import Navbar from "./components/Navbar.tsx";
 import Footer from "./components/Footer.tsx";
 import Home from "./components/Home";
 import Product from "./components/Product.tsx";
+import Cart from './components/Cart.tsx';
+import Checkout from './components/Checkout.tsx';
+import Payment from './components/Payment.tsx';
+import ConfirmPayment from './components/ConfirmPayment.tsx';
 
 function App() {
   return (
@@ -12,6 +16,10 @@ function App() {
         <Routes>
           <Route path="/" Component={Home} />
           <Route path="/product" Component={Product} />
+          <Route path="/cart" Component={Cart} />
+          <Route path="/checkout" Component={Checkout} />
+          <Route path="/payment" Component={Payment} />
+          <Route path="/confirm" Component={ConfirmPayment} />
         </Routes>
         <Footer />
       </div>
